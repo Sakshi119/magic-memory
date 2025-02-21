4b0a6c24214f0f326588e026ca0441c844cdcf91
Sakshi Sen <sakshi_sen@ildc-inlp.idealake.com>
2025-02-21 09:07:51 +0530

    cards display and shuffle function added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 
+import { useState } from 'react';
 import './App.css';
 
+
+const cardImages=[
+  {"src":"/img/card1.png"},
+  {"src":"/img/card2.png"},
+  {"src":"/img/card3.png"},
+  {"src":"/img/card4.png"},
+  {"src":"/img/card5.png"},
+  {"src":"/img/card6.png"}
+]
 function App() {
+
+const [cards,setCards]= useState([])
+const [turns,setTurns]= useState(0)
+
+
+//shuffle cards
+  const shuffleCards =()=>{
+    const shuffledCards = [...cardImages, ...cardImages].sort(()=> Math.random() - 0.5).map((card)=>({...card,id:Math.random()}))
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards,turns)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
